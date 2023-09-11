a5ce1d4966ec99ac56de320f06794f4f9f734c39
Ben Purinton <ben.purinton@gmail.com>
2023-09-11 10:19:39 -0800

    specs passing

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,7 +2,7 @@
 #
 # Table name: venues
 #
-#  id           :bigint           not null, primary key
+#  id           :integer          not null, primary key
 #  address      :string
 #  name         :string
 #  neighborhood :string
